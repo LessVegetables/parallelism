1dc62b85fde4d449aa1a07a3ebb56db9f40e7db1
LessVegetables <57289239+LessVegetables@users.noreply.github.com>
2025-02-15 08:32:59 +0700

    Lab1
    Ready for review

---
+++ 1/1.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <math.h>
+
+using namespace std;
+
+#ifndef TYPE
+#define TYPE double
+#endif
+
+
+int main()
+{
+    std::cout << "TYPE set to: " << typeid(TYPE).name() << std::endl;
+
+
+    vector<TYPE> arr;
+
+    #define MY_PI 3.1415926536
+    
+    // 0.000000628318531
+    TYPE step = (2.0 * MY_PI) / 10000000.0;
+    TYPE alpha = 0.0;
+
+    for (int i = 0; i < 10000000; i++)
+    {
+        arr.push_back(sin(alpha));
+        alpha = i * step;
+        // alpha = i * 2.0 * MY_PI / 10000000.0; // alpha += step;
+    }
+    // cout << "2pi: " << alpha << "\npi: " << alpha / 2.0 << endl;
+
+    TYPE sum = 0.0;
+    for (int i = 0; i < 10000000; i++)
+    {
+        sum += arr[i];
+    }
+    // cout << arr.size() << endl;
+    cout << sum << endl;
+    return 0;
+}
+
+// g++ daniel-1.cpp -o daniel
