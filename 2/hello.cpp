94a7d200ee4fd240c9aeb889d36bfebbb5581dc7
LessVegetables <57289239+LessVegetables@users.noreply.github.com>
2026-05-07 11:42:46 +0700

    i dont have adhd i swear

--- 2/hello.cpp
+++ 2/hello.cpp
@@ -5,7 +5,7 @@
 int main (int argc, char **argv){
     #pragma omp parallel    /* <-- Fork */
     {
-        std::cout << "Hello, multithreaded world: thread " << omp_get_thread_num() << " of " << omp_get_num_threads()) << std::endl;
+        std::cout << "Hello, multithreaded world: thread " << omp_get_thread_num() << " of " << omp_get_num_threads() << std::endl;
     }   /* <-- Barrier & join */
     return 0;
 }
